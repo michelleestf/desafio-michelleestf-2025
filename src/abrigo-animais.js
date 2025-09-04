ed2f5b7d92c062e74cc81f6e1297f77d654dcc44
Michelle Estefanir <104026755+michelleestf@users.noreply.github.com>
2025-09-04 17:52:01 -0300

    adicionando dados dos animais, validação de entrada, ordem, e regra de adoção

--- src/abrigo-animais.js
+++ src/abrigo-animais.js
@@ -1,6 +1,30 @@
+const animais = {
+  rex: { tipo: "cão", brinquedos: ["RATO","BOLA"]},
+  mami: { tipo: "gato", brinquedos: ["BOLA","LASER"]},
+  fofo: {tipo:"gato", brinquedos: ["BOLA","RATO","LASER"]},
+  zero: {tipo: "gato", brinquedos: ["RATO","BOLA"]},
+  bola: {tipo: "cão", brinquedos: ["CAIXA","NOVELO"]},
+  bebe: {tipo: "cão", brinquedos: ["LASER","RATO","BOLA"]},
+  loco: {tipo: "jabuti", brinquedos: ["SKATE","RATO"]}
+};
+
 class AbrigoAnimais {
 
   encontraPessoas(brinquedosPessoa1, brinquedosPessoa2, ordemAnimais) {
+    
+    // validação
+    const { erro, brinquedos1, brinquedos2, ordem } = validarEntradas( brinquedosPessoa1, brinquedosPessoa2, ordemAnimais, animais);
+
+    if(erro) {
+      return {erro};
+    }
+    
+    // regra de adoção
+    const resultado = ordemLista.map((animalNome) =>
+      definirAdoção(animalNome, brinquedos1, brinquedos2, animais)
+    );
+
+    return {lista: resultado.sort()};
   }
 }
 
